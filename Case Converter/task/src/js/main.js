c612f7794dfd2d4fcab97b653c2e82b33efe6558
Ahmad Shalabi <eng.ahmadshalabi@gmail.com>
2021-09-05 00:39:05 +0300

    Use classes instead of element to style components

--- Case Converter/task/src/js/main.js
+++ Case Converter/task/src/js/main.js
@@ -1,4 +1,4 @@
-const textArea = document.querySelector('textarea');
+const textArea = document.getElementsByClassName("textarea");
 
 const toUpperCaseButton = document.getElementById('upper-case');
 toUpperCaseButton.addEventListener('click', function () {
